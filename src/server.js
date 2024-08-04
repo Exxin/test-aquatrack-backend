9e99990b0cbf65208c29ffd3f5107161acc44351
Exxin <exxinblood@gmail.com>
2024-08-04 23:59:19 +0300

    ааа

--- src/server.js
+++ src/server.js
@@ -14,15 +14,9 @@ import { swaggerDocs } from './middlewares/swaggerDocs.js';
 
 const PORT = Number(env('PORT', '3000'));
 
-//дозволені домени, з яких можна робити запити
+// Дозволені домени, з яких можна робити запити
 const allowedOrigins = [
-  //локалхост для тестування
-  // 'http://localhost:5173',
-  // 'http://localhost:3000',
-
-  //деплой-продакшен
-  // 'https://test-aquatrack.vercel.app',
-  '*'
+  'https://test-aquatrack.vercel.app',
 ];
 
 const corsOptions = {
@@ -38,35 +32,14 @@ const corsOptions = {
 
 export const setupServer = () => {
   const app = express();
-  app.use(cors({
-  origin: function (origin, callback) {
-    if (allowedOrigins.includes(origin) || !origin) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-  credentials: true,
-}));
-  app.use(express.json());
-  app.use(express.urlencoded({ extended: true }));
-  app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://test-aquatrack.vercel.app');
-  res.header('Access-Control-Allow-Credentials', 'true');
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH,OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  next();
-  });
-  // app.use(cors());
-  app.use(cors({
-    origin: 'https://test-aquatrack.vercel.app',
-    credentials: true,
-    preflightContinue: true,
-  }));
-  app.use(express.json());
-  app.use(express.urlencoded({ extended: true }));
+
+  // Налаштування CORS
   app.use(cors(corsOptions));
+
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: true }));
   app.use(cookieParser());
+
   app.use('/uploads', express.static(UPLOAD_DIR));
   app.use('/api-docs', swaggerDocs());
 
@@ -75,7 +48,7 @@ export const setupServer = () => {
       transport: {
         target: 'pino-pretty',
         options: {
-          colorize: true, // приклад опції для кольорового виведення
+          colorize: true,
         },
       },
     }),
@@ -84,11 +57,9 @@ export const setupServer = () => {
   app.use(router);
 
   app.use('*', notFoundHandler);
-
   app.use(errorHandler);
 
   app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
   });
-  app.options('*', cors(corsOptions));
 };
