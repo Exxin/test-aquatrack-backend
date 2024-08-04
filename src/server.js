a44be90be606d388b44589fd236f7777583c0426
Exxin <exxinblood@gmail.com>
2024-08-04 23:37:31 +0300

    ф

--- src/server.js
+++ src/server.js
@@ -15,15 +15,26 @@ import { swaggerDocs } from './middlewares/swaggerDocs.js';
 const PORT = Number(env('PORT', '3000'));
 
 //дозволені домени, з яких можна робити запити
-// const allowedOrigins = [
-//   //локалхост для тестування
-//   'http://localhost:5173',
-//   'http://localhost:3000',
+const allowedOrigins = [
+  //локалхост для тестування
+  'http://localhost:5173',
+  'http://localhost:3000',
 
-//   //деплой-продакшен
-//   'https://test-aquatrack.vercel.app',
-//   'https://test-aquatrack-backend.onrender.com',
-// ];
+  //деплой-продакшен
+  'https://test-aquatrack.vercel.app',
+  'https://test-aquatrack-backend.onrender.com',
+];
+
+const corsOptions = {
+  origin: (origin, callback) => {
+    if (allowedOrigins.includes(origin) || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true,
+};
 
 export const setupServer = () => {
   const app = express();
@@ -36,14 +47,16 @@ export const setupServer = () => {
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH,OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   next();
-});
+  });
   // app.use(cors());
   app.use(cors({
     origin: 'https://test-aquatrack.vercel.app',
     credentials: true,
     preflightContinue: true,
   }));
-
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: true }));
+  app.use(cors(corsOptions));
   app.use(cookieParser());
   app.use('/uploads', express.static(UPLOAD_DIR));
   app.use('/api-docs', swaggerDocs());
@@ -68,4 +81,5 @@ export const setupServer = () => {
   app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
   });
+  app.options('*', cors(corsOptions));
 };
