43eddc19c783d61013a0b81ff05c1ffd4c4aba58
Exxin <exxinblood@gmail.com>
2024-08-06 16:35:06 +0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -19,7 +19,7 @@ const allowedOrigins = [
   'http://localhost:5173',
   'http://localhost:3000',
   'https://test-aquatrack.vercel.app',
-  'https://test-aquatrack-c71h.vercel.app',
+  'https://aquatrack.vercel.app',
   'https://test-aquatrack-backend.onrender.com',
 ];
 
