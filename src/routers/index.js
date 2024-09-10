83d50048835b1885ed4d58676d4201652e5cf19f
Exxin <exxinblood@gmail.com>
2024-09-10 17:30:33 +0300

    Update index.js

--- src/routers/index.js
+++ src/routers/index.js
@@ -5,11 +5,13 @@ import { Router } from 'express';
 import waterRouter from './water.js';
 import usersRouter from './users.js';
 import authRouter from './auth.js';
+import getGoogleAuthUrl from '../controllers/auth.js';
 
 const router = Router();
 
 router.use('/auth', authRouter);
 router.use('/users', usersRouter);
 router.use('/water', waterRouter);
+router.use('/get-oauth-url', getGoogleAuthUrl);
 
 export default router;
